666a93c769430788b2b4afb1417ead1b471e2191
Denis Maina <Denis.maina@student.moringaschool.com>
2022-08-18 15:41:35 +0300

    commit tree ded0b1cc48ab6de2da0604e740eba3f16fa3e7b6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    #setter for the name attribute
+    def name=(name)
+        @name = name
+    end
 
+    #getter for the name attribute
+    def name
+        @name
+    end
+    #setter for the breed attribute
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #getter for the breed attribute
+    def breed
+        @breed
+    end
 end
